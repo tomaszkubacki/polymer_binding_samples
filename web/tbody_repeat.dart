6954f6510125eec2f0f7ab575375bd2891164dd7
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2014-03-30 22:00:35 +0200

    tbody repeat binding

---
+++ web/tbody_repeat.dart
@@ -0,0 +1,29 @@
+import 'package:polymer/polymer.dart';
+
+
+@CustomTag('tbody-repeat')
+class TbodyRepeat extends PolymerElement {
+  @published int count = 0;
+
+  TbodyRepeat.created() : super.created() {
+  }
+  
+  @observable List<Item> items = toObservable([new Item("A item","A item description"),
+                                               new Item("B item","B item description"),
+                                               new Item("C item","C item description"),
+                                               new Item("D item","D item description")]);
+ 
+}
+
+
+
+class Item extends Observable {
+  
+  @observable String name;
+  
+  @observable String description;
+  
+  Item(this.name,this.description);
+  
+}
+
