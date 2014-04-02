beb5a1f230f70f4b29da437f455597ed9e61b56e
Reto Glauser <reto.glauser@blinkeye.ch>
2014-04-02 15:38:34 +0200

    nested template repeats to reflect List<List<Item>> data structure

--- web/tbody_repeat.dart
+++ web/tbody_repeat.dart
@@ -1,25 +1,42 @@
 import 'package:polymer/polymer.dart';
-
+import 'dart:async';
 
 @CustomTag('tbody-repeat')
 class TbodyRepeat extends PolymerElement {
-  
-  TbodyRepeat.created() : super.created() {
-  }
-  
-  @observable List<Item> items = toObservable([new Item("A item","A item description"),
-                                               new Item("B item","B item description"),
-                                               new Item("C item","C item description"),
-                                               new Item("D item","D item description")]);
+
+   TbodyRepeat.created() : super.created() {
+   }
+
+   @observable List<List<Item>> items = toObservable([
+      [new Item("A item","A item description"), new Item("A1 item","A1 item description"), new Item("A2 item","A2 item description")],
+      [new Item("B item","B item description"), new Item("B2 item","B2 item description"), new Item("B2 item","B2 item description")],
+      [new Item("C item","C item description")],
+      [new Item("D item","D item description")]
+   ]);
+
+   var timer = null;
+
+   @override
+      enteredView() {
+         if( timer == null ) {
+            timer = new Timer.periodic(new Duration(seconds: 3), updateList);
+         }
+      }
+
+   void updateList(Timer _) {
+      DateTime today = new DateTime.now();
+      String time = today.hour.toString() + ":" + today.minute.toString() + "." + today.second.toString();
+      items.add( [new Item(time, today.millisecondsSinceEpoch.toString() + today.hour.toString() + ":" + today.minute.toString() + "." + today.second.toString() )] );
+      items.first.first.description = today.millisecondsSinceEpoch.toString();
+   }
 }
 
 class Item extends Observable {
-  
-  @observable String name;
-  
-  @observable String description;
-  
-  Item(this.name,this.description);
-  
+
+   @observable String name;
+
+   @observable String description;
+
+   Item(this.name,this.description);
 }
 
